5d9672f61a8ec81054b7dee7b0b2d887a04d1d77
Paul Desmond Parker <pauldesmondparker@gmail.com>
2017-12-09 17:52:17 +0800

    Add dependencies to package.yaml. Add Wai route.
    Wai route serves the simple elm app we created earlier on localhost:8000
    as spa/index.html

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
+import Network.Wai
+import Network.HTTP.Types
+import Network.Wai.Handler.Warp (run)
+
+
+app2 :: Application
+app2 _ respond = respond index
+
+index :: Response
+index = responseFile
+    status200
+    [("Content-Type", "text/html")]
+    "spa/index.html"
+    Nothing
 
 main :: IO ()
-main = someFunc
+main = do
+  putStrLn $ "localhost:8000"
+  run 8000 app2
